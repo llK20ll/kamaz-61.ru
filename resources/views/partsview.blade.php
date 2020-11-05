ba1fe1af597c5bf666e881cddb3c0b6ef2d20185
llK20ll <43817392+llK20ll@users.noreply.github.com>
2020-11-05 23:47:15 +0300

    правки
    правки

--- resources/views/partsview.blade.php
+++ resources/views/partsview.blade.php
@@ -5,39 +5,40 @@
 @section('content')
 
 
-<div class="p-0 mr-3 mt-3 mb-3 w-100">
+<div class="p-3 mr-3  w-100 h-100">
     <h3 class="text-center">{{$parts->title}}</h3>
     <div class="tbl-content-view bg-light">
-    <table class="table">
-        <th class="text-right">Код запчасти</th>
-        <th class="text-right">Наименование</th>
-        <tbody>
-            @isset($parts->parts_tab)                               
-                       
-            <?php
-            $string = $parts->parts_tab;
-            $separator = "\t\n";
-            
-            $array_words = [];
-            $tok = strtok($string, $separator);
-            
-            while($tok) {
-                $array_words[] = $tok;
-                $tok = strtok($separator);
-            }
-            // var_dump($array_words);
-            $array = array_chunk($array_words, 2); ?>
-                <?php foreach ($array as $items): ?>
-                    <tr>
-                    <?php foreach ($items as $row): ?>
-                        <td class="text-right"><?php echo $row; ?></td>
+        <table class="table">
+            <th class="text-right">Код запчасти</th>
+            <th class="text-right">Наименование</th>
+            <tbody>
+                @isset($parts->parts_tab)                               
+                        
+                <?php
+                $string = $parts->parts_tab;
+                $separator = "\t\n";
+                
+                $array_words = [];
+                $tok = strtok($string, $separator);
+                
+                while($tok) {
+                    $array_words[] = $tok;
+                    $tok = strtok($separator);
+                }
+                // var_dump($array_words);
+                $array = array_chunk($array_words, 2); ?>
+                    <?php foreach ($array as $items): ?>
+                        <tr>
+                        <?php foreach ($items as $row): ?>
+                            <td class="text-right"><?php echo $row; ?></td>
+                        <?php endforeach; ?>
+                        </tr>
                     <?php endforeach; ?>
-                    </tr>
-                <?php endforeach; ?>
-            @endisset                          
-          
-        </tbody>
-    </table>
-    </div></div>
+                @endisset                          
+            
+            </tbody>
+        </table>
+    </div>
+</div>
 
 @endsection
