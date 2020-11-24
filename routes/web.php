04468ef4c624be0dc316597eedc593512b2ddb9e
llK20ll <43817392+llK20ll@users.noreply.github.com>
2020-11-24 23:04:05 +0300

    title

--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,7 @@ Route::group(['prefix' => 'basket',], function () {
 
 Route::get('/{category}','App\Http\Controllers\MainController@category')->name('category');
 Route::get('/{category}/{product}', 'App\Http\Controllers\MainController@product')->name('product');
+Route::get('/sitemap.xml', 'App\Http\Controllers\SitemapController@index');
 
 
 
