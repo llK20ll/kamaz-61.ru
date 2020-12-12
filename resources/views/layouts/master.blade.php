0d68581dedbd506bbed496bf101ad1c61bb72d1c
llK20ll <43817392+llK20ll@users.noreply.github.com>
2020-12-12 22:48:11 +0300

    Update master.blade.php
    метрика

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -231,20 +231,27 @@
                     </div>            
                 </div>
                 
-                <!-- Yandex.Metrika counter -->
-                <script type="text/javascript" >
-                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                
-                    ym(69086911, "init", {
-                        clickmap:true,
-                        trackLinks:true,
-                        accurateTrackBounce:true
-                    });
-                </script>
-                <noscript><div><img src="https://mc.yandex.ru/watch/69086911" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-                <!-- /Yandex.Metrika counter -->
+                            <!-- Yandex.Metrika informer -->
+                            <a href="https://metrika.yandex.ru/stat/?id=33824834&amp;from=informer"
+                            target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/33824834/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+                            style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="33824834" data-lang="ru" /></a>
+                            <!-- /Yandex.Metrika informer -->
+
+                            <!-- Yandex.Metrika counter -->
+                            <script type="text/javascript" >
+                            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                            ym(33824834, "init", {
+                                    clickmap:true,
+                                    trackLinks:true,
+                                    accurateTrackBounce:true,
+                                    webvisor:true
+                            });
+                            </script>
+                            <noscript><div><img src="https://mc.yandex.ru/watch/33824834" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+                            <!-- /Yandex.Metrika counter -->
             </footer>
         </div>
 
